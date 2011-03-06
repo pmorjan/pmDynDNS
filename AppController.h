fc9c57306ff5e27ccfdf7004fb4b4c0c84255800
Peter Morjan <pmorjan@gmail.com>
2011-03-06 13:51:40 +0000

    git-svn-id: file:///opt/svn/repos/pmDynDNS/trunk@241 96b230d0-e0c1-4e4d-b396-6d38b3a0f7fd

--- AppController.h
+++ AppController.h
@@ -6,19 +6,22 @@
 #import <Cocoa/Cocoa.h>
 #import "AppController.h"
 
-@interface AppController : NSObject {
-    NSString        *ipDNS;
-    NSString        *ipCurrent;
-    NSImage         *icon;
-    NSString        *hostname;
+@interface AppController : NSObject <NSApplicationDelegate> {
+    IBOutlet NSWindow *window;
+    NSString *ipDNS;
+    NSString *ipCurrent;
+    NSImage  *icon;
+    NSString *hostname;
     IBOutlet NSTextField *urlLabel;
     IBOutlet NSButton *button;
     IBOutlet NSProgressIndicator *progress;
 }
 
-@property (copy)        NSString *ipDNS;
-@property (copy)        NSString *ipCurrent;
-@property (retain)      NSImage  *icon;
+@property (assign)  NSWindow *window;
+@property (copy)    NSString *ipDNS;
+@property (copy)    NSString *ipCurrent;
+@property (retain)  NSImage  *icon;
 @property (copy)    NSString *hostname;
+
 - (IBAction)checkDNS:(id)sender;
 @end
